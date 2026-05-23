60491aec31bd286cac51e6f2017912766680840d
BeeJa01 <joshuababs2005@gmail.com>
2026-05-23 10:08:34 +0100

    update footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -96,7 +96,7 @@ const Footer = () => {
               title: "Legal",
               links: [
                 { label: "Privacy Notice", to: "/privacy" },
-                { label: "Cookie Policy", to: "/cookies-policy" },
+                { label: "Cookie Policy", to: "/cookie-policy" },
                 { label: "Terms & Conditions", to: "/terms" },
                 { label: "Preferences", to: "/preferences" },
               ],
